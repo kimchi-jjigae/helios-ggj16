9653e80697a60faff9df32e4b81f6ad663642f4f
Kim Wong <kimspindel@gmail.com>
2016-01-29 23:16:20 +0100

    add main scene and rotater script

--- Assets/scripts/Rotater.cs
+++ Assets/scripts/Rotater.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Rotater : MonoBehaviour {
 
+    public float mRotationSpeed;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +12,6 @@ public class Rotater : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        transform.Rotate(new Vector3(0, 0, mRotationSpeed));
 	}
 }
