5453f73d735696d9f35dc784e6f137805691a0a6
Kim Wong <kimspindel@gmail.com>
2016-01-30 17:39:30 +0100

    trying to fix the helios physics

--- Assets/scripts/HeliosController.cs
+++ Assets/scripts/HeliosController.cs
@@ -4,24 +4,39 @@ using System.Collections;
 public class HeliosController : MonoBehaviour {
 
     Rigidbody2D mRigidbody2d;
-    public float mSpeedScalar;
-    Vector2 mScreenBounds;
+    public float mForceScalar;
+    //Vector2 mPositionBounds;
+    // position offset stuff //
+    float mLimitX = 0.1f;
+    float mLimitY = 0.3f;
+    float mTimeFactorX = 1.0f;
+    float mTimeFactorY = 2.0f;
+
+    Vector2 mPositionBeforeOffset;
 
 	void Start() {
-        mScreenBounds = new Vector2(10.0f, 10.0f);
+        //mScreenBounds = new Vector2(10.0f, 10.0f);
         mRigidbody2d = GetComponent<Rigidbody2D>();
+
+        mPositionBeforeOffset = mRigidbody2d.position;
 	}
 	
 	void FixedUpdate() {
+        mRigidbody2d.position = mPositionBeforeOffset;
 	    float hMovement = Input.GetAxis("Horizontal");
 	    float vMovement = Input.GetAxis("Vertical");
-        Vector2 force = new Vector2(hMovement * mSpeedScalar, vMovement * mSpeedScalar);
-        mRigidbody2d.AddForce(force * 100.0f);
-        //mRigidbody2d.velocity = force;
+        Vector2 force = new Vector2(hMovement, vMovement);
+        mRigidbody2d.AddForce(force * mForceScalar);
+        mPositionBeforeOffset = mRigidbody2d.position;
+
+        float offsetX = mLimitX * Mathf.Sin(Time.time * mTimeFactorX);
+        float offsetY = mLimitY * Mathf.Cos(Time.time * mTimeFactorY);
+        Vector2 positionOffset = new Vector2(offsetX, offsetY);
+        mRigidbody2d.position += positionOffset;
 
         /*
-        float xPos = Mathf.Clamp(mRigidbody2d.position.x, -screenBounds.x, screenBounds.x);
-        float yPos = Mathf.Clamp(mRigidbody2d.position.y, -screenBounds.y, screenBounds.y);
+        float xPos = Mathf.Clamp(mRigidbody2d.position.x, -mPositionBounds.x, mPositionBounds.x);
+        float yPos = Mathf.Clamp(mRigidbody2d.position.y, -mPositionBounds.y, mPositionBounds.y);
         mRigidbody2d.position = new Vector2(xPos, yPos);
         */
 	}
---
+++ Assets/scripts/ObjectCollisionHandler.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectCollisionHandler : MonoBehaviour {
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D other) {
+        /*
+        Health health = other.GetComponent<Health>();
+        health.damage(1);
+        */
+
+        Debug.Log("KROCK BANG PANG");
+        Destroy(gameObject);
+    }
+}
